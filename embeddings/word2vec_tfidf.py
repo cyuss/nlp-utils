ae86179fc732c0e34619b7e92f03fa040766e8c8
Cyuss <youcef@Cyuss.local>
2019-04-02 22:46:25 +0200

    better handling for word2vec arguments

--- embeddings/word2vec_tfidf.py
+++ embeddings/word2vec_tfidf.py
@@ -19,25 +19,17 @@ class MySentences:
 
 # word2vec class
 class Word2vecTfIdfEmbedding:
-    def __init__(self, size=100, window=5, min_count=5, max_vocab_size=None, sample=.001, workers=3, negative=5, alpha=.025):
+    def __init__(self, *args, **kwargs):
         self.word2weight = None
         # model parameters
-        self.size = size # vectors dimension
-        self.window = window # the maximum distance between the current and the predicted word within a sentence
-        self.min_count = min_count # ignore all words with total frequency lower than this
-        self.max_vocab_size = max_vocab_size # set the vocabulary size
-        self.sample = sample # the threshold for configuring which higher-frequency words are randomly downsampled
-        self.workers = workers # define how many workers
-        self.negative = negative # specifies how many “noise words” should be drawn
-        self.alpha = alpha # the initial learning rate
+        self.args = args
+        self.kwargs = kwargs
+        self.size = size[0] if args else kwargs["size"] # vectors dimension
 
     def fit(self, X, y=None):
         self.sentences = X
         # train word2vec embedding
-        self.word2vec = gensim.models.Word2Vec(MySentences(self.sentences), min_count=self.min_count, size=self.size,
-                                              window=self.window, max_vocab_size=self.max_vocab_size,
-                                              sample=self.sample, negative=self.negative,
-                                              alpha=self.alpha, workers=self.workers)
+        self.word2vec = gensim.models.Word2Vec(MySentences(self.sentences), *self.args, **self.kwargs)
         self.model = dict(zip(self.word2vec.wv.index2word, self.word2vec.wv.vectors))
         # train tf-idf vectorizer
         tfidf = TfidfVectorizer(analyzer=lambda x: x)
@@ -45,7 +37,7 @@ class Word2vecTfIdfEmbedding:
         # store the coefficients
         max_idf = max(tfidf.idf_)
         self.word2weight = defaultdict(
-            lambda: max_idf,
+            lambda: max_idf, # default value for the defaultdict
             [(w, tfidf.idf_[i]) for w, i in tfidf.vocabulary_.items()]
         )
         return self
